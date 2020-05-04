3bf2d79b5cc0b02296abbcdd001954f61b945558
JaggKerby <ilyakerby@gmail.com>
2020-05-04 13:48:41 +0300

    finaly

---
+++ scrypt.js
@@ -0,0 +1,79 @@
+
+
+// const age = prompt( 'Enter your age: ');
+
+// if (age > 18 && age < 75) {
+//     alert( 'Access Granted');
+// } else if (age < 18) { 
+//     alert ('Too young');
+// } else {
+//     alert ('Too old');
+// }   
+
+
+// const sym = prompt ('Enter your gender M or F: ');
+ 
+// const gender = (sym === 'M' ) ? 'Male' : 'Female' ;
+
+// console.log (gender);
+
+
+// let num = +prompt ('Enter a number: ' );
+
+// switch (num) {
+//     case 0:
+//         alert( 0 );
+//         break;  
+//     case 1:
+//         alert( 1 );
+//         break;
+//     case 2:
+//     case 3:
+//         alert( '2,3');
+//         break;
+//     default: 
+//         alert('another number');
+// }
+
+// const a = 12; 
+// const b = 3;
+
+// if ( a > b ) {
+//     console.log ( a );
+// } else {
+//     console.log ( b );
+// }
+
+// const num = +prompt ( 'Enter a number from 1 to 12: ');
+
+// switch (num) {
+//     case 1: 
+//         alert('January');
+//         break;
+//     case 2:
+//         alert('February');
+//         break;
+//     case 3:
+//         alert('March');
+//         break;
+//     case 4:
+//         alert('April');
+//         break;
+//     case 5:
+//         alert('May');
+//         break;
+// }
+
+// const circle = 19.65;
+// const square = 25;
+
+// const line = Math.sqrt(square); 
+// const findround = circle/Math.PI; 
+
+// const diametr = 2*Math.sqrt(findround);
+
+// if (diametr <= line ) {
+//     alert('Круг влезет в квадрат');
+// } else {
+//     alert ('Круг больше квадрата')
+// }
